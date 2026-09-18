1bc5dfe6592c717ccfbe4a11bdc1a8b79e3221d3
fdef54983-del <fdef54983@gmail.com>
2026-09-18 17:54:46 -0400

    fix: restore existing Ely.by OAuth configuration

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ElyByLoginFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/ElyByLoginFragment.java
@@ -2,26 +2,15 @@ package net.kdt.pojavlaunch.fragments;
 
 import net.kdt.pojavlaunch.extra.ExtraConstants;
 
-import git.artdeell.mojo.BuildConfig;
-
 public class ElyByLoginFragment extends OAuthFragment {
     public static final String TAG = "ELYBY_LOGIN_FRAGMENT";
-
     public ElyByLoginFragment() {
         super("internalredirect",
-                buildAuthorizationUrl(),
+                "https://account.ely.by/oauth2/v1" +
+                        "?client_id=mojolauncher2" +
+                        "&redirect_uri=internalredirect%3A%2F%2Fcomplete" +
+                        "&response_type=code" +
+                        "&scope=account_info%20offline_access%20minecraft_server_session",
                 ExtraConstants.ELYBY_LOGIN_TODO);
-    }
-
-    private static String buildAuthorizationUrl() {
-        String clientId = BuildConfig.ELYBY_CLIENT_ID;
-        if(clientId.isEmpty()) {
-            clientId = "franyu-unconfigured";
-        }
-        return "https://account.ely.by/oauth2/v1" +
-                "?client_id=" + clientId +
-                "&redirect_uri=internalredirect%3A%2F%2Fcomplete" +
-                "&response_type=code" +
-                "&scope=account_info%20offline_access%20minecraft_server_session";
     }
 }
