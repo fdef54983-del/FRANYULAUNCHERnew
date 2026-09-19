366aa901785da6170e4703cfe2e547542dc870f3
fdef54983-del <fdef54983@gmail.com>
2026-09-19 13:31:26 -0400

    fix: resume downloaded update after permission screen

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -246,6 +246,7 @@ public class MainMenuFragment extends Fragment {
     public void onResume() {
         super.onResume();
         ExtraCore.setValue(ExtraConstants.REFRESH_ACCOUNT_SPINNER, true);
+        if (mUpdateChecker != null) mUpdateChecker.resumePendingInstall(requireActivity());
         if (getView() != null) {
             refreshSelectedInstance(getView());
             renderRecentInstances();
